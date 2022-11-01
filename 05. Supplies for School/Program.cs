edf7bb791fe06790830c3b260a7ca5f5e68a5219
User <User@DESKTOP-ESP2JT1>
2022-11-01 12:13:00 +0200

    Add project files.

---
+++ 05. Supplies for School/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Supplies_for_School
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+
+            int penPakets = int.Parse(Console.ReadLine());
+            int markerPakets = int.Parse(Console.ReadLine());
+            int litresBoardCleaner = int.Parse(Console.ReadLine());
+            int percentDiscount = int.Parse(Console.ReadLine());
+
+            // calculate
+
+            double pricePenPakets = penPakets * 5.80;
+            double priceMarkerPakets = markerPakets * 7.20;
+            double priceLitresBoardCleaner = litresBoardCleaner * 1.20;
+            double pricePecentDiscount = pricePenPakets + priceMarkerPakets + priceLitresBoardCleaner;
+            double priceWithDiscount = pricePecentDiscount - (pricePecentDiscount * percentDiscount / 100);
+
+            // output
+
+            Console.WriteLine(priceWithDiscount);
+            
+        }
+    }
+}
